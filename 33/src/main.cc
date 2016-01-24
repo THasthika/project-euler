eddf58bd15287bc57d79e01f467021c1d4ba63ba
Tharindu Hasthika <tharindubathigama@gmail.com>
2016-01-24 10:14:27 +0530

    34

--- 33/src/main.cc
+++ 33/src/main.cc
@@ -2,14 +2,36 @@
 
 using namespace std;
 
+int gcd(int p, int q)
+{
+  if(p == 0) return q;
+  int r = p % q;
+  return gcd(r, p);
+}
+
 int main(int argc, char **argv)
 {
+  int denproduct = 1;
+  int numproduct = 1;
 
-	float x;
+  for(int i = 1; i < 10; i++)
+  {
+    for(int den = 1; den < i; den++)
+    {
+      for(int num = 1; num < den; num++)
+      {
+        if((den * (10 * num + i)) == (num * (10 * i + den)))
+        {
+          denproduct *= den;
+          numproduct *= num;
+        }
+      }
+    }
+  }
 
-	x = (float)1 / (float)6;
+  denproduct /= gcd(numproduct, denproduct);
 
-	cout << x << endl;
+  cout << denproduct << endl;
 
 	return 0;
 }
