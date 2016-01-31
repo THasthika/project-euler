3dfb1ba7383804ed15e0c63d18ca704be2fe280a
Tharindu Hasthika <tharindubathigama@gmail.com>
2016-01-31 11:26:22 +0530

    48

---
+++ 48/inc/main.h
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#ifndef _MAIN_H
+#define _MAIN_H
+
+using namespace std;
+
+#endif
---
+++ 48/src/main.cc
@@ -0,0 +1,24 @@
+#include "main.h"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	long result = 0;
+	long modulo = 10000000000;
+
+	for (int i = 1; i <= 1000; i++) {
+	    long temp = i;
+	    for (int j = 1; j < i; j++) {
+	        temp *= i;
+					temp %= modulo;
+	    }
+
+	    result += temp;
+	    result %= modulo;
+	}
+
+	cout << result << endl;
+
+	return 0;
+}
